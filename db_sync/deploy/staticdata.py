d880b0d63d1bf9005e04dc8d64d06a86751f6c67
DraganMatesic <dragan.matesic@gmail.com>
2024-01-07 12:36:57 +0100

    basic database data
    - adding ORM table for storing basic database data regarding schema and tables
    - adding class method get_all_tables that prepares data for ORM table ManageTables
    - table.py removed, change of approach

--- db_sync/deploy/staticdata.py
+++ db_sync/deploy/staticdata.py
@@ -1,15 +1,23 @@
-from db_sync.deploy import tables
+from db_sync import deploy
 from db_sync.utilities import functions
 
 
 def event_type_data():
     hash_columns = ['event_id', 'description']
-    event_type_codes = [tables.EventType(event_id='I', description='insert'),
-                        tables.EventType(event_id='U', description='update'),
-                        tables.EventType(event_id='D', description='delete'),
-                        tables.EventType(event_id='R', description='reloading the entire table to the node')]
+    event_type_codes = [deploy.tables.EventType(event_id='I', description='insert'),
+                        deploy.tables.EventType(event_id='U', description='update'),
+                        deploy.tables.EventType(event_id='D', description='delete'),
+                        deploy.tables.EventType(event_id='R', description='reloading the entire table to the node')]
     functions.db_hash(event_type_codes, hash_columns)
     return event_type_codes
+
+
+def database_table_data(records: list[dict]):
+    hash_columns = ['server_id', 'database_name', 'database_type', 'schema_name', 'table_name']
+    [record.update({'empty_schema': 1}) for record in records if record.get('table_name') is None]
+    db_tables = [deploy.tables.ManageTables(**record) for record in records]
+    functions.db_hash(db_tables, hash_columns)
+    return db_tables
 
 
 def sync_tables_data():
